759218c0c9c8c0811684ec88d7ef77a9de5a6d7c
MemphisThoth <jared.perkins@hotmail.com>
2024-12-27 13:31:55 -0500

    Created netlify.toml

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 const { DateTime } = require("luxon"); 
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/assets/");
+    
     eleventyConfig.addFilter("customDate", dateObj => {
         return DateTime.fromJSDate(dateObj).toFormat("MM-dd-yyyy");
     });
